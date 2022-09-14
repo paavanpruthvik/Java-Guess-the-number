e4019a8fa51a349d7dd02642a3415abeb09ae3ca
Paavan <109504234+paavanpruthvik@users.noreply.github.com>
2022-09-14 15:48:06 +0530

    Add files via upload

---
+++ Guess.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Guess 
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		boolean correct = false;
+		Random ra = new Random();
+		int randno = ra.nextInt(10);
+		// System.out.println(randno);
+			
+		while(!correct)
+		{
+			System.out.print("Guess the number: ");
+			int guessedno = sc.nextInt();
+			if(guessedno == randno)
+				{
+					System.out.println(guessedno +" is the right number!");
+					correct = true;
+				}
+				else if(guessedno > randno)
+				{
+					System.out.println("Actual Number is lower than "+guessedno);
+				}
+				else if(guessedno < randno)
+				{
+					System.out.println("Actual Number is higher than "+guessedno);
+				}		
+		}	
+	}	
+}
